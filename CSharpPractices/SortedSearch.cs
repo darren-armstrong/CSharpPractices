ab1b3d5e3b8f0209550539633528c292aac64bbc
Darren Armstrong <armstrong.d1990@gmail.com>
2017-06-21 14:49:20 +0100

    Sorted Search example using TDD
    Created a soted search algorithm to find how many number are less than a certain number.  Used a binary search algorithm to speed up the search and reduce the number of nodes visited.

---
+++ CSharpPractices/SortedSearch.cs
@@ -0,0 +1,30 @@
+﻿
+namespace CSharpPractices
+{
+    class SortedSearch
+    {
+        public static int CountNumbers(int[] sortedArray, int lessThan)
+        {
+            int min = 0;
+            int max = sortedArray.Length - 1;
+            int mid = 0;
+            while (min <= max)
+            {
+                if (sortedArray[max] < lessThan)
+                {
+                    return max + 1;
+                }
+                mid = (min + max) / 2;
+                if (sortedArray[mid] < lessThan)
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid - 1;
+                }
+            }
+            return mid;
+        }
+    }
+}
---
+++ CSharpPractices/Tests/SortedSearchTest.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+
+namespace CSharpPractices.Tests
+{
+    public class SortedSearchTest
+    {
+        [Fact]
+        public void NoValuesInTheArrayLessThan()
+        {
+            Assert.Equal(0, SortedSearch.CountNumbers(new int[] { 5, 6, 7, 8 }, 4));
+        }
+
+        [Fact]
+        public void ValuesInTheArrayLessThan()
+        {
+            Assert.Equal(2, SortedSearch.CountNumbers(new int[] { 1, 3, 5, 7 }, 4));
+        }
+
+        [Fact]
+        public void ValuesInTheLongArrayLessThan()
+        {
+            Assert.Equal(11, SortedSearch.CountNumbers(new int[] { 1, 3, 5, 7, 8, 9, 10, 11, 12, 13, 14, 16 }, 15));
+        }
+
+        [Fact]
+        public void ValuesInTheSmallArrayLessThan()
+        {
+            Assert.Equal(2, SortedSearch.CountNumbers(new int[] { 1, 3}, 4));
+        }
+
+        [Fact]
+        public void ReturnZeroIfEmptyListIsPassedThrough()
+        {
+            Assert.Equal(0, SortedSearch.CountNumbers(new int[] {}, 4));
+        }
+
+        [Fact]
+        public void ValueInTheArrayLessThan()
+        {
+            Assert.Equal(1, SortedSearch.CountNumbers(new int[] { 1 }, 4));
+        }
+
+        [Fact]
+        public void ValueNotInTheArrayLessThan()
+        {
+            Assert.Equal(0, SortedSearch.CountNumbers(new int[] { 4 }, 1));
+        }
+
+        [Fact]
+        public void MaxValueInArrayIsLessThan()
+        {
+            Assert.Equal(9, SortedSearch.CountNumbers(new int[] { 1,2,3,4,5,6,7,8,9 }, 10));
+        }
+    }
+}

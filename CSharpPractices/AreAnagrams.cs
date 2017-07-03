8baf4d4874f6cef1760729662e4b825cf8a8b0b5
Darren Armstrong <armstrong.d1990@gmail.com>
2017-07-03 09:28:45 +0100

    check for anagrams algorithm
    alogirthm that checks for anagrams

---
+++ CSharpPractices/AreAnagrams.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharpPractices
+{
+    public class AreAnagrams
+    {
+        public static bool AreStringsAnagrams(string a, string b)
+        {
+            if (string.IsNullOrEmpty(a) || string.IsNullOrEmpty(b))
+                return false;
+            if (a.Length != b.Length)
+                return false;
+
+            foreach (var c in b)
+            {
+                var index = a.IndexOf(c);
+                if (index >= 0)
+                    a = a.Remove(index, 1);
+                else
+                    return false;
+            }
+
+            return string.IsNullOrEmpty(a);
+        }
+    }
+}
